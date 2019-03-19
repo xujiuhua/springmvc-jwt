12412d75058ceb876c064602226d8a6ae7880377
jiuhua.xu <xujiuhuamoney@163.com>
2019-03-19 16:06:31 +0800

    :art: 再次优化一下

--- src/main/java/com/jing/tong/controllers/UserController.java
+++ src/main/java/com/jing/tong/controllers/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
     @PostMapping("/login")
     @ResponseBody
     public ResponseData login(@RequestParam String username, @RequestParam String password) {
